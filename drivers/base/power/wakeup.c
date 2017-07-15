7afbb178c8677c1346ea85004ad5931ced3aead7
Nathan Chancellor <natechancellor@gmail.com>
2017-07-15 13:40:22 -0700

    drivers: wakeup: force disable several wakelocks
    Change-Id: Ib1fa01e4c3d83976de6d39feb4f3023ef842631c
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -18,24 +18,24 @@
 #include <trace/events/power.h>
 #include <linux/moduleparam.h>
 
-static bool enable_si_ws = true;
+#include "power.h"
+
+static bool enable_si_ws = false;
 module_param(enable_si_ws, bool, 0644);
-static bool enable_msm_hsic_ws = true;
+static bool enable_msm_hsic_ws = false;
 module_param(enable_msm_hsic_ws, bool, 0644);
-static bool enable_wlan_rx_wake_ws = true;
+static bool enable_wlan_rx_wake_ws = false;
 module_param(enable_wlan_rx_wake_ws, bool, 0644);
-static bool enable_wlan_ctrl_wake_ws = true;
+static bool enable_wlan_ctrl_wake_ws = false;
 module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
-static bool enable_wlan_wake_ws = true;
+static bool enable_wlan_wake_ws = false;
 module_param(enable_wlan_wake_ws, bool, 0644);
-static bool enable_bluedroid_timer_ws = true;
+static bool enable_bluedroid_timer_ws = false;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
 static bool enable_timerfd_ws = true;
 module_param(enable_timerfd_ws, bool, 0644);
 static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
-
-#include "power.h"
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
